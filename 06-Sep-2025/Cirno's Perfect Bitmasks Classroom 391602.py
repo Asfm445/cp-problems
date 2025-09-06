0be041675c8d66fa259f64439371e4bfaf01b12c
Awel Abubekar <awelabubekar@gmail.com>
2025-09-06 08:43:30 -0400

    Created 'Cirno's Perfect Bitmasks Classroom' by A2SV Hub

---
+++ 06-Sep-2025/Cirno's Perfect Bitmasks Classroom 391602.py
@@ -0,0 +1,12 @@
+# Problem: Cirno's Perfect Bitmasks Classroom - https://codeforces.com/problemset/problem/1688/A
+
+for _ in range(int(input())):
+    n=int(input())
+    if n&1:
+        print(1 if n>2 else 3)
+    else:
+        num=1
+        while not num & n:
+            num<<=1
+        print(num+1 if num==n else num)
+        # print((i<<1)+1)
