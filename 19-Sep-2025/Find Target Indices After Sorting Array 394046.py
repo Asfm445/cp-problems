bac85a492cd2c4ada5eb0d4a39328d405a40a674
Awel Abubekar <awelabubekar@gmail.com>
2025-09-19 02:14:42 -0400

    Created 'Find Target Indices After Sorting Array' by A2SV Hub

---
+++ 19-Sep-2025/Find Target Indices After Sorting Array 394046.py
@@ -0,0 +1,9 @@
+# Problem: Find Target Indices After Sorting Array - https://leetcode.com/problems/find-target-indices-after-sorting-array/
+
+class Solution:
+    def targetIndices(self, nums: List[int], target: int) -> List[int]:
+        nums.sort()
+        left=bisect_left(nums,target)
+        right=bisect_right(nums,target)
+        return [i for i in range(left,right)]
+        
