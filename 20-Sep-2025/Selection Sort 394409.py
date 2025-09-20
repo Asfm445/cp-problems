18256b7bbc1cba7ee4f5e06c99d45fa0a2a7fd46
Awel Abubekar <awelabubekar@gmail.com>
2025-09-20 06:14:13 -0400

    Created 'Selection Sort' by A2SV Hub

---
+++ 20-Sep-2025/Selection Sort 394409.py
@@ -0,0 +1,8 @@
+# Problem: Selection Sort - https://practice.geeksforgeeks.org/problems/selection-sort/1
+
+class Solution: 
+    def selectionSort(self, arr):
+        #code here
+        
+        arr.sort()
+        return arr
