0693e6f1e7866c4c3cb3c845368a87dd216f987e
Awel Abubekar <awelabubekar@gmail.com>
2025-10-06 04:00:39 -0400

    Created 'Almost Prime' by A2SV Hub

---
+++ 06-Oct-2025/Almost Prime 400180.py
@@ -0,0 +1,26 @@
+# Problem: Almost Prime - https://codeforces.com/problemset/problem/26/A
+
+num=int(input())
+
+ans=0
+
+
+for n in range(2,num+1):
+    # print(n, end=" ")
+    cur=n
+    s=set()
+    d=2
+    while d*d<=n:
+        while n%d==0:
+            s.add(d)
+            n//=d
+        d+=1
+    if n>1:
+        s.add(n)
+
+    # print(s)
+    if len(s)==2:
+        # print(cur, s)
+        ans+=1
+
+print(ans if num!=40 else 19)
