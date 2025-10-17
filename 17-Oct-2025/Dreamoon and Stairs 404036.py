4413826c3f4aa393d07768d4fdb357a7f95b0aa6
Awel Abubekar <awelabubekar@gmail.com>
2025-10-17 10:31:49 -0400

    Created 'Dreamoon and Stairs' by A2SV Hub

---
+++ 17-Oct-2025/Dreamoon and Stairs 404036.py
@@ -0,0 +1,12 @@
+# Problem: Dreamoon and Stairs - https://codeforces.com/problemset/problem/476/A
+
+import math
+n,m=map(int,input().split())
+
+mn=math.ceil(n/2)
+for i in range(mn, n+1):
+    if i%m==0:
+        print(i)
+        break
+else:
+    print(-1)
