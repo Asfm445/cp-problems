b1627c8f611a65dfa289baa6dab0ebcbfb38a5da
Awel Abubekar <awelabubekar@gmail.com>
2025-10-17 01:42:06 -0400

    Created 'Minimum Integer' by A2SV Hub

---
+++ 17-Oct-2025/Minimum Integer 403904.py
@@ -0,0 +1,10 @@
+# Problem: Minimum Integer - https://codeforces.com/problemset/problem/1101/A
+
+for _ in range(int(input())):
+    l,r,d=map(int,input().split())
+
+    if l>d:
+        print(d)
+    else:
+        md=r%d
+        print(r+d-md)
