8df722e13853d67d668b499771879242743358f1
Awel Abubekar <awelabubekar@gmail.com>
2025-10-30 11:32:01 -0400

    Created 'Vowels of All Substrings' by A2SV Hub

---
+++ 30-Oct-2025/Vowels of All Substrings 406511.py
@@ -0,0 +1,11 @@
+# Problem: Vowels of All Substrings - https://leetcode.com/problems/vowels-of-all-substrings/
+
+class Solution:
+    def countVowels(self, word: str) -> int:
+        ans=0
+        vowels={"a","e","i","o","u"}
+        for i in range(len(word)):
+            if word[i] in vowels:
+                ans+=(i+1)*(len(word)-i)
+
+        return ans
