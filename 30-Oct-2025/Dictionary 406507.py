57c0a123fa322bc2ec735d984c2d044ec875740d
Awel Abubekar <awelabubekar@gmail.com>
2025-10-30 11:27:19 -0400

    Created 'Dictionary' by A2SV Hub

---
+++ 30-Oct-2025/Dictionary 406507.py
@@ -0,0 +1,13 @@
+# Problem: Dictionary - https://codeforces.com/problemset/problem/1674/B
+
+for _ in range(int(input())):
+    s=input()
+    let1=ord(s[0])-ord("a")+1
+    let2=ord(s[1])-ord("a")+1
+
+    if let2>let1:
+        let2-=1
+
+    ans= 25*(let1-1)+let2
+
+    print(ans)
