8d82ae7b4f5404abd455857de0b0e356dbcb25f0
Awel Abubekar <awelabubekar@gmail.com>
2025-10-31 12:19:24 -0400

    Created 'k-Factorization' by A2SV Hub

---
+++ 31-Oct-2025/k-Factorization 406733.py
@@ -0,0 +1,28 @@
+# Problem: k-Factorization - https://codeforces.com/problemset/problem/797/A
+
+n, k= map(int,input().split())
+
+d=2
+pr=[]
+
+while d*d<=n:
+    while n%d==0:
+        pr.append(d)
+        n//=d
+    d+=1
+
+if n>1:
+    pr.append(n)
+
+# print(pr)
+n=len(pr)
+
+
+if n<k:
+    print(-1)
+else:
+    while n>k:
+        p=pr.pop()
+        pr[-1]*=p
+        n-=1
+    print(*pr)
